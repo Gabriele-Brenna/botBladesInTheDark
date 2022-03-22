cfdff22d49519b6af92da039b25a86854fb2e148
Niccolo-Bergamaschi <niccolo.bergamaschi@mail.polimi.it>
2022-03-22 19:24:04 +0100

    Added SpecialAbility.py and Upgrade.py

---
+++ main/Upgrade.py
@@ -0,0 +1,8 @@
+class Upgrade:
+    def __init__(self, name: str, quality: int):
+        self.name = name
+        self.quality = quality
+
+    def __str__(self) -> str:
+        return """Upgrade name: {}
+        Quality: {}""".format(self.name, self.quality)
