1a146591017403cef07e79e52b632a384efaf66a
LeandroBognanni <leandro.bognanni@mail.polimi.it>
2022-03-25 18:58:50 +0100

    Added Clock.py.

---
+++ main/Clock.py
@@ -0,0 +1,18 @@
+class Clock:
+
+    def __init__(self, name: str = "", segments: int = 4, progress: int = 0) -> None:
+        self.name = name
+        self.segments = segments
+        self.progress = progress
+
+    def tick(self, ticks: int) -> bool:
+        self.progress += ticks
+        if self.progress < 0:
+            self.progress = 0
+        return self.progress >= self.segments
+
+    def edit(self, name: str = None, segments: int = None):
+        if name is not None:
+            self.name = name
+        if segments is not None:
+            self.segments = segments
